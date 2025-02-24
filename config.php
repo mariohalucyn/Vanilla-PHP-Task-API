20b298da28da7a1bf78413f2fc1b6c2759dfd26f
Kacper Gajda <kgajda226@gmail.com>
2025-02-24 11:19:46 +0100

    minor changes

--- config.php
+++ config.php
@@ -4,6 +4,7 @@ require_once 'vendor/autoload.php';
 
 use Dotenv\Dotenv;
 
+
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
@@ -11,6 +12,7 @@ ini_set('display_errors', 1);
 ini_set('session.cookie_secure', 1);
 ini_set('session.cookie_httponly', 1);
 ini_set('session.use_only_cookies', 1);
+
 session_start();
 session_regenerate_id();
 error_reporting(E_ALL);
