12b12064a4a0e3a19e762389d04731ce881e9e73
Kacper Gajda <kgajda226@gmail.com>
2025-02-26 11:45:24 +0100

    prevent logout for users that currently logged out

--- src/Controllers/Auth/AuthenticatedSessionController.php
+++ src/Controllers/Auth/AuthenticatedSessionController.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace App\Controllers\Auth;
 
 use App\Controllers\Controller;
@@ -11,15 +10,14 @@ use Exception;
 class AuthenticatedSessionController extends Controller {
     public function store(array $request): void {
         try {
-            $email = strtolower(trim($request['email'] ?? ''));
-            $password = trim($request['password'] ?? '');
-
             if (empty($email) || empty($password)) {
                 throw new Exception('All fields are required', 422);
             }
 
-            $user = new User()->findByEmail($email);
+            $email = strtolower(trim($request['email']));
+            $password = trim($request['password']);
 
+            $user = new User()->findByEmail($email);
             if (!$user || !password_verify($password, $user['password'])) {
                 throw new Exception('Invalid email or password', 422);
             }
@@ -39,6 +37,13 @@ class AuthenticatedSessionController extends Controller {
     }
 
     public function destroy(): void {
+        if (!isset($_SESSION['id'])) {
+            Response::json([
+                'success' => false,
+                'message' => 'No user is currently logged in'
+            ], 401);
+        }
+
         session_unset();
         session_destroy();
 
